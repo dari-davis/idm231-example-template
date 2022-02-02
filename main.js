f7aef71675566cc715a291c617f8e29954a9487b
Darielle Davis <hello@darielledavis.com>
2022-02-01 20:55:11 -0500

    Updates main display image on button click

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
+//
+//
+//
+//
+function fade2Content() {
+    console.log('fade2Content called');
+    document.body.classList.remove("blackScreen");
+}
+
+const displayObj = document.getElementById('display');
 const trumpetBtn = document.getElementById('trumpet');
 const saxophoneBtn = document.getElementById('saxophone');
 const guitarBtn = document.getElementById('guitar');
-const bottomDiv = document.getElementById('bottom');
 
 trumpetBtn.addEventListener('click', function() {
+    //console.log('trumpet button clicked');
     userPicked('trumpet');
 });
 
@@ -15,8 +25,29 @@ guitarBtn.addEventListener('click', function() {
     userPicked('guitar');
 });
 
-// create function userPicked();
-function userPicked(instrument) {
-    console.log(`The user picked a ${instrument}`);
-    //window.alert(`The user picked a ${instrument}`);
+function userPicked(buttonName) {
+    //console.log('User picked ' + buttonName);
+    // swap out large image for button related image
+    // show the description
+    // play sound
+
+    switch (buttonName) {
+        case 'trumpet':
+            console.log('trumpet was clicked');
+            displayObj.src = 'img/trumpet.png';
+            displayObj.classList.add('visible');
+        break;
+        case 'saxophone':
+            console.log('saxophone was clicked');
+            displayObj.src = 'img/saxophone.png';
+        break;
+        case 'guitar':
+            console.log('guitar was clicked');
+            displayObj.src = 'img/guitar.png';
+        break;
+        default:
+            console.log('A button was clicked!');
+    }
+
 }
+
